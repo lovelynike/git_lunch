f1b22651f6c12a852da19d4935f698b77aab0bb1
Jake <lovelynike@naver.com>
2023-08-07 11:26:07 +0900

    git directions_202308071126

--- git_revert.java
+++ git_revert.java
@@ -2,6 +2,6 @@ public class git_revert {
     public static void main(String args[]){
         System.out.println("revert");
         System.out.println("revert_01_이걸 revert 해보자 ");
-        
+        System.out.println("revert_위에 한글로 적은거 revert 해봐 ");
     }
 }
