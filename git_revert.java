03d5eb866bdbfa45164811f7e574c21543c97471
Jake <lovelynike@naver.com>
2023-08-07 13:40:50 +0900

    git directions_202308071340

--- git_revert.java
+++ git_revert.java
@@ -4,6 +4,6 @@ public class git_revert {
         System.out.println("revert_01_이걸 revert 해보자 ");
 
         System.out.println("revert_02_이걸 revert 어렵다. restore나 해봐 ");
-        System.out.println("revert_03_이걸 restore나 해봐 지금 커밋 한번 할건데 지금 커밋 싫어 2번으로 돌아갈래 ");
+        
     }
 }
---
+++ git_test_c.java
@@ -0,0 +1,3 @@
+public class git_test_c {
+    
+}
